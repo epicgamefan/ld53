e3e17a426a01765a04673a3febe22550278fb619
Joshua Luoni <josh.luoni@gmail.com>
2023-04-28 21:20:09 -0400

    adding an initial scene that transistion to game afater 10 seconds.

---
+++ Assets/SceneChange.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChange : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float delay = 10;
+    public string NextScene = "GameScene";
+
+    void Start()
+    {
+        StartCoroutine(LoadLevelAfterDelay(delay));
+    }
+
+    IEnumerator LoadLevelAfterDelay(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        SceneManager.LoadScene(NextScene);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
