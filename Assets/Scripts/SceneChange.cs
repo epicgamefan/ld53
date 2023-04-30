284debfed87e72c04e14e2b1c2b8419f728f3104
Joshua Luoni <josh.luoni@gmail.com>
2023-04-30 10:57:09 -0400

    hacked together a quick title screen.

--- Assets/Scripts/SceneChange.cs
+++ Assets/Scripts/SceneChange.cs
@@ -6,19 +6,34 @@ using UnityEngine.SceneManagement;
 public class SceneChange : MonoBehaviour
 {
     // Start is called before the first frame update
-    public float delay = 10;
+    //public float delay = 10;
     public string NextScene = "GameScene";
 
     void Start()
     {
-        StartCoroutine(LoadLevelAfterDelay(delay));
+        //StartCoroutine(LoadLevelAfterDelay(delay));
     }
 
-    IEnumerator LoadLevelAfterDelay(float delay)
+    public void StartGame()
     {
-        yield return new WaitForSeconds(delay);
         SceneManager.LoadScene(NextScene);
     }
+
+    public void ExitGame()
+    {
+
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
+        Application.Quit();
+       
+    }
+
+    //IEnumerator LoadLevelAfterDelay(float delay)
+    //{
+    //    yield return new WaitForSeconds(delay);
+    //    SceneManager.LoadScene(NextScene);
+    //}
 
     // Update is called once per frame
     void Update()
