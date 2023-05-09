76de46401760097bf9c654d8d075bd8084fb6665
Joshua Luoni <josh.luoni@gmail.com>
2023-05-09 00:11:44 -0400

    fix some fine tuning issues.

--- Assets/Scripts/Collision.cs
+++ Assets/Scripts/Collision.cs
@@ -19,12 +19,15 @@ public class Collision : MonoBehaviour
     public bool onSpikes;
 
     [Space]
-
+    //public float collisionRadius = 0.25f;
+    //public Vector2 bottomOffset, rightOffset, leftOffset;
     [Header("Collision")]
+    public bool showLeftGizmo, showRightGizmo, showBottomGizmo, showSpikesGizmo;
+    public Vector2 leftBoxOffset, leftBoxSize;
+    public Vector2 rightBoxOffset, rightBoxSize;
+    public Vector2 bottomBoxOffset, bottomBoxSize;
+    public Vector2 spikesBoxOffset, spikesBoxSize;
 
-    public float collisionRadius = 0.25f;
-    public Vector2 bottomOffset, rightOffset, leftOffset;
-    private Color debugCollisionColor = Color.red;
 
     // Start is called before the first frame update
     void Start()
@@ -36,19 +39,27 @@ public class Collision : MonoBehaviour
     void Update()
     {
 
-        onSpikes = Physics2D.OverlapCircle((Vector2)transform.position + bottomOffset, collisionRadius, spikesLayer)
-            || Physics2D.OverlapCircle((Vector2)transform.position + rightOffset, collisionRadius, spikesLayer)
-            || Physics2D.OverlapCircle((Vector2)transform.position + leftOffset, collisionRadius, spikesLayer);
+        //onSpikes = Physics2D.OverlapCircle((Vector2)transform.position + bottomOffset, collisionRadius, spikesLayer)
+        //    || Physics2D.OverlapCircle((Vector2)transform.position + rightOffset, collisionRadius, spikesLayer)
+        //    || Physics2D.OverlapCircle((Vector2)transform.position + leftOffset, collisionRadius, spikesLayer);
+
+        onSpikes = Physics2D.OverlapBox((Vector2)transform.position + spikesBoxOffset, spikesBoxSize, 0, spikesLayer);
 
         if (!onSpikes)
         {
-            onGround = Physics2D.OverlapCircle((Vector2)transform.position + bottomOffset, collisionRadius, groundLayer);
+            //onGround = Physics2D.OverlapCircle((Vector2)transform.position + bottomOffset, collisionRadius, groundLayer);
+            onGround = Physics2D.OverlapBox((Vector2)transform.position + bottomBoxOffset, bottomBoxSize, 0, groundLayer);
 
-            onWall = Physics2D.OverlapCircle((Vector2)transform.position + rightOffset, collisionRadius, groundLayer)
-                || Physics2D.OverlapCircle((Vector2)transform.position + leftOffset, collisionRadius, groundLayer);
+            //onWall = Physics2D.OverlapCircle((Vector2)transform.position + rightOffset, collisionRadius, groundLayer)
+            //    || Physics2D.OverlapCircle((Vector2)transform.position + leftOffset, collisionRadius, groundLayer);
+            onWall = Physics2D.OverlapBox((Vector2)transform.position + rightBoxOffset, rightBoxSize, 0, groundLayer)
+                    || Physics2D.OverlapBox((Vector2)transform.position + leftBoxOffset, leftBoxSize, 0, groundLayer);
 
-            onRightWall = Physics2D.OverlapCircle((Vector2)transform.position + rightOffset, collisionRadius, groundLayer);
-            onLeftWall = Physics2D.OverlapCircle((Vector2)transform.position + leftOffset, collisionRadius, groundLayer);
+
+            //onRightWall = Physics2D.OverlapCircle((Vector2)transform.position + rightOffset, collisionRadius, groundLayer);
+            onRightWall = Physics2D.OverlapBox((Vector2)transform.position + rightBoxOffset, rightBoxSize, 0, groundLayer);
+            //onLeftWall = Physics2D.OverlapCircle((Vector2)transform.position + leftOffset, collisionRadius, groundLayer);
+            onLeftWall = Physics2D.OverlapBox((Vector2)transform.position + leftBoxOffset, leftBoxSize, 0, groundLayer);
 
             wallSide = onRightWall ? -1 : 1;
         }
@@ -65,10 +76,27 @@ public class Collision : MonoBehaviour
     {
         Gizmos.color = Color.red;
 
-        var positions = new Vector2[] { bottomOffset, rightOffset, leftOffset };
+        // var positions = new Vector2[] { bottomOffset, rightOffset, leftOffset };
+        if (showBottomGizmo)
+        {
+            Gizmos.DrawWireCube((Vector2)transform.position + bottomBoxOffset, bottomBoxSize);
+        }
+        if (showRightGizmo)
+        {
+            Gizmos.DrawWireCube((Vector2)transform.position + rightBoxOffset, rightBoxSize);
+        }
+        if (showLeftGizmo)
+        {
+            Gizmos.DrawWireCube((Vector2)transform.position + leftBoxOffset, leftBoxSize);
+        }
 
-        Gizmos.DrawWireSphere((Vector2)transform.position  + bottomOffset, collisionRadius);
-        Gizmos.DrawWireSphere((Vector2)transform.position + rightOffset, collisionRadius);
-        Gizmos.DrawWireSphere((Vector2)transform.position + leftOffset, collisionRadius);
+        if (showSpikesGizmo)
+        {
+            Gizmos.DrawWireCube((Vector2)transform.position + spikesBoxOffset, spikesBoxSize);
+        }
+
+        //Gizmos.DrawWireSphere((Vector2)transform.position  + bottomOffset, collisionRadius);
+        //Gizmos.DrawWireSphere((Vector2)transform.position + rightOffset, collisionRadius);
+        //Gizmos.DrawWireSphere((Vector2)transform.position + leftOffset, collisionRadius);
     }
 }
